e012d41616bd75d23bfb1ba0241edebae42638bc
Nima <www.nimamahdi1385@gmail.com>
2023-10-09 16:24:57 +0330

    Set up mongoose in nodejs , Set up views , Set up public folder for html css js and etc , Move files to public and views

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Set up mongoDB connection
+mongoose.connect(process.env.MONGO_CONNECTION)
+    .then(() => {
+        console.log('Mongo connection open');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// public folder for css js font and etc.
+app.use(express.static('public'));
 
 // Create home route to see is server working or not
 app.get('/' , (req ,res) => {
