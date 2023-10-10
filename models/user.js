d9fe9537ab06d01db872fd71dc3f4c881b83e3bb
Nima <www.nimamahdi1385@gmail.com>
2023-10-10 19:46:51 +0330

    adding authentication stuff

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String
+    }
+});
+
+const model = mongoose.model('User', userSchema);
+
+module.exports = model;
